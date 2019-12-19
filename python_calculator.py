2e29b382415fb197b70b06c8ed0c54167d06b3c9
fora22 <fora22@naver.com>
2019-12-20 05:09:43 +0900

    calculator start

---
+++ python_calculator.py
@@ -0,0 +1,25 @@
+import re
+
+class calculator():  # Calculator 객체 선언
+    def __init__(self):
+        self.result = []  #
+
+    def add(self, x, y):
+        add_result = x + y
+        return add_result
+
+    def substract(self, x, y):
+        substract_result = x - y
+        return substract_result
+
+    def multifly(self, x, y):
+        multifly_result = x * y
+        return multifly_result
+
+    def divide(self, x, y):
+        divide_result = x / y
+        return divide_result
+
+p = re.compile("[a-z]+")
+m = p.search("3 python")
+print(m)
