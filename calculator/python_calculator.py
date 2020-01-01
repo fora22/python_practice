bdb2ecbfb18defd0c6ec3a684ede18e93e968857
fora22 <fora22@naver.com>
2020-01-01 18:37:05 +0900

    객체(class)로 변환중 ver.1

--- calculator/python_calculator.py
+++ calculator/python_calculator.py
@@ -8,13 +8,28 @@ class node:
 
 class calculator():  # Calculator 객체 선언
     def __init__(self):
+        self.mathmatical_expression
         self.stack = []
         self.postfix_notation = []
         self.operator = ["*", "/", "+", "-"]
         self.bracket = ["(", ")"]
 
+    def input_infix(self, input_infix):
+        p = re.compile("\W|\d+")
+        self.mathmatical_expression = p.findall("2+3*(5+4*2)-3+6")  # 2+3*(5+4*2)-3+6
+        self.mathmatical_expression = self.delete_blank(self.mathmatical_expression)
+
+    def delete_blank(self, m_e):
+        i = 0
+        while i != len(m_e):
+            if m_e[i] == " ":
+                m_e.pop(i)
+                i = i - 1
+            i = i + 1
+        return m_e
+
     def is_number(self, x):
-        if x not in operator and x not in bracket:
+        if x not in self.operator and x not in self.bracket:
             return True
         else:
             return False
@@ -28,63 +43,42 @@ class calculator():  # Calculator 객체 선언
             return -1
 
     def change_postfix(self, mathmatical_expression):
-
         for m_e in mathmatical_expression:
             if self.is_number(m_e):
                 self.postfix_notation.append(m_e)
-            elif m_e in operator:
+            elif m_e in self.operator:
                 prime_number = self.priority_judge(m_e)
                 while len(self.stack) > 0:
-                    top_of_stack = stack[-1]
+                    top_of_stack = self.stack[-1]
                     if self.priority_judge(top_of_stack) < prime_number:  # 우선순위 구분이 이상함
                         break
-                    while len(stack) > 0:
-                        #     temp = stack.pop()
-                        #     if temp == "(":
-                        #         break
-                        #     postfix_notation.append(temp)
-                        self.postfix_notation.append(stack.pop())
+                    while len(self.stack) > 0:
+                        self.postfix_notation.append(self.stack.pop())
                 self.stack.append(m_e)
 
             elif m_e == "(":
-                stack.append(m_e)
+                self.stack.append(m_e)
             elif m_e == ")":
                 while True:
-                    temp = stack.pop()
+                    temp = self.stack.pop()
                     if temp == "(":
                         break
-                    postfix_notation.append(temp)
+                    self.postfix_notation.append(temp)
 
-        while len(stack) > 0:
-            postfix_notation.append(stack.pop())
-
-
-    def add(self, x, y):
-        add_result = x + y
-        return add_result
-
-    def substract(self, x, y):
-        substract_result = x - y
-        return substract_result
-
-    def multifly(self, x, y):
-        multifly_result = x * y
-        return multifly_result
-
-    def divide(self, x, y):
-        divide_result = x / y
-        return divide_result
+        while len(self.stack) > 0:
+            self.postfix_notation.append(self.stack.pop())
 
 
+    def operation(self, x, y, oper):
+        if oper == "+":
+            return x + y
+        elif oper == "-":
+            return x - y
+        elif oper == "*":
+            return x * y
+        elif oper == "/":
+            return x / y
 
-def delete_blank(test):
-    i = 0
-    while i != len(test):
-        if test[i] == " ":
-            test.pop(i)
-            i = i -1
-        i = i + 1
-    return test
 
 # def is_number(str):
 #     try:
@@ -99,117 +93,10 @@ def delete_blank(test):
 
 
 
-stack = []
-postfix_notation = []
-operator = ["*", "/", "+", "-"]
-bracket = ["(", ")"]
-
-p = re.compile("\W|\d+")
-mathmatical_expression = p.findall("2+3*(5+4*2)-3+6") #2+3*(5+4*2)-3+6
-mathmatical_expression = delete_blank(mathmatical_expression)
-
-for m_e in mathmatical_expression:
-    if is_number(m_e):
-        postfix_notation.append(m_e)
-    elif m_e in operator:
-        prime_number = priority_judge(m_e)
-        while len(stack) > 0:
-            top_of_stack = stack[-1]
-            if priority_judge(top_of_stack) < prime_number:            # 우선순위 구분이 이상함
-                break
-            while len(stack) > 0:
-            #     temp = stack.pop()
-            #     if temp == "(":
-            #         break
-            #     postfix_notation.append(temp)
-                postfix_notation.append(stack.pop())
-        stack.append(m_e)
-
-    elif m_e == "(":
-        stack.append(m_e)
-    elif m_e == ")":
-        while True:
-            temp = stack.pop()
-            if temp == "(":
-                break
-            postfix_notation.append(temp)
-
-while len(stack) > 0:
-    postfix_notation.append(stack.pop())
-
-print(stack)
-print(postfix_notation)
-
-
-# def count_bracket(math_exp):        # 괄호 개수를 세는 함수
-#     count_left = 0
-#     count_right = 0
-#     for m_e in math_exp:
-#         if m_e == "(":
-#             count_left = count_left + 1
-#         elif m_e == ")":
-#             count_right = count_right + 1
-#
-#     if count_right == count_left:
-#         return count_right
-#     else:
-#         print("수식이 잘못 입력되었습니다.")
-#         return -1
-#
-# count = count_bracket(mathmatical_expression)
-# stack = [[] for x in range(count + 1)]          # comprehension을 활용한 satck list
-# postfix_notation = [[] for x in range(count + 1)]
-
-
-
-'''
-number = 0
-for m_e in mathmatical_expression:          # 후위 표기법 작성
-    if m_e == "(":
-        number = number + 1
-    elif m_e == ")":
-        for p_n in postfix_notation[number]:
-            postfix_notation[number-1].append(p_n)
-        number = number - 1
-    elif is_number(m_e):
-        postfix_notation[number].append(m_e)
-    else:
-        if (m_e == "*") or (m_e == "/"):
-            if (stack[number][-1] == "*") or (stack[number][-1] == "/"):
-                stack[number].append(m_e)
-            elif (stack[number][-1] == "+") or (stack[number][-1] == "-"):
-                postfix_notation[number] = postfix_notation[number] + stack[number]
-        elif (m_e == "+") or (m_e == "-"):
-            # 여기부터 해야되는데...
-            stack[number].append(m_e)
-'''
-
-
-
-# for m in mathmatical_expression:
-#     if is_number(m):
-#         postfix_notation.append(m)
-#     else:
-#         stack.append(m)
 
 
 
 
 
-# for i in range(len(t)):
-#     if t[i].isdecimal():
-#         postfix_notation.append(t[i])
-#     elif t[i] == ')':
-#         for j in range(len(stack)):
-#             postfix_notation.append(stack[j])
-#         del stack[:]
-#         postfix_notation.remove("(")
-#     elif i == (len(t)-1):
-#         for j in range(len(stack)):
-#             postfix_notation.append(stack[j])
-#         del stack[:]
-#     else:
-#         stack.append(t[i])
-#
-# print(t)
+
 
